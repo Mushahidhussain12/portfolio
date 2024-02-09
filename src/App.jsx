1547c46182c1589ccaac98b444d668b1eba05db4
mushahid hussain <khokharmushahidhussain@gmail.com>
2024-02-09 19:21:28 +0500

    i

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Contact from "./Components/Contact"
 import Navigation from "./Components/Navigation"
 import Projects from "./Components/Projects"
 import Skills from "./Components/Skills"
-import Footer from "./Components/footer"
+import Footer from "./Components/Footer"
 import Hero from "./Components/Hero"
 function App() {
   return (
